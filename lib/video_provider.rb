667fa53aa85eb928d64e3895d03d8b2f6033284b
nirnaor <nirnaori@gmail.com>
2016-09-01 16:28:02 +0300

    Added category id to find only music videos.

--- lib/video_provider.rb
+++ lib/video_provider.rb
@@ -1,7 +1,7 @@
 # Youtube video provider
 class VideoProvider
   def search(name)
-    url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{name}&order=viewCount&type=video&key=AIzaSyChE-vTXxk4iqcXjTOtD_JVKawo7Cw3ueU"
+    url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{name}&order=viewCount&videoCategoryId=10&type=video&key=AIzaSyChE-vTXxk4iqcXjTOtD_JVKawo7Cw3ueU"
     resp = HTTParty.get(url).parsed_response
     resp['items'].map { |v| v['id']['videoId'] }.first
   end
