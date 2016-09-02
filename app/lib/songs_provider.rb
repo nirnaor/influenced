b6c52b95363dde9d4f54695970b9187dff7608a2
nirnaor <nirnaori@gmail.com>
2016-09-02 16:13:13 +0300

    Saving artist name from the Spotify API.

--- app/lib/songs_provider.rb
+++ app/lib/songs_provider.rb
@@ -4,12 +4,15 @@ class SongsProvider
     # Get Id.
     url = "https://api.spotify.com/v1/search?q=#{name}&type=artist"
     res = HTTParty.get(url).parsed_response
-    id = res["artists"]["items"].first["id"]
+
+    first = res["artists"]["items"].first
+    id = first["id"]
+    name = first["name"]
 
     # Get top tracks of artist Id.
     t_trl = "https://api.spotify.com/v1/artists/#{id}/top-tracks?country=US"
     resp = HTTParty.get(t_trl).parsed_response
-    resp['tracks'].map { |s| s["name"] }
+    { name: name, songs: resp['tracks'].map { |s| s["name"] } }
   end
 
 end
--- app/lib/video_provider.rb
+++ app/lib/video_provider.rb
@@ -1,11 +1,12 @@
 # Youtube video provider
 class VideoProvider
   def search(name)
-    song = SongsProvider.new.search(name).sample
+    data = SongsProvider.new.search(name)
+    song = data[:songs].sample
     query = "#{name} #{song}"
     url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{query}&order=viewCount&videoCategoryId=10&type=video&key=AIzaSyChE-vTXxk4iqcXjTOtD_JVKawo7Cw3ueU"
     resp = HTTParty.get(url).parsed_response
     video = resp['items'].first 
-    { videoid: video['id']['videoId'], title: video["snippet"]["title"]}
+    data.merge({ videoid: video['id']['videoId'], title: video["snippet"]["title"]})
   end
 end
