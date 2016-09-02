a9d2f1a1ecc19e35da442789eafb293f50c727b3
nirnaor <nirnaori@gmail.com>
2016-09-02 16:50:25 +0300

    Displaying image of artists.

--- app/lib/songs_provider.rb
+++ app/lib/songs_provider.rb
@@ -5,14 +5,16 @@ class SongsProvider
     url = "https://api.spotify.com/v1/search?q=#{name}&type=artist"
     res = HTTParty.get(url).parsed_response
 
-    first = res["artists"]["items"].first
-    id = first["id"]
-    name = first["name"]
+    artist_data = res["artists"]["items"].first
+    name = artist_data['name']
+    id = artist_data["id"]
+    image = artist_data['images'][1]['url']
+
 
     # Get top tracks of artist Id.
     t_trl = "https://api.spotify.com/v1/artists/#{id}/top-tracks?country=US"
     resp = HTTParty.get(t_trl).parsed_response
-    { name: name, songs: resp['tracks'].map { |s| s["name"] } }
+    { name: name, image: image }.merge({ songs: resp['tracks'].map { |s| s["name"] } })
   end
 
 end
